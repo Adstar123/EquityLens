f243bd440d609e10b89a957692f17411f39438d7
Adstar123 <80363480+Adstar123@users.noreply.github.com>
2026-03-29 20:27:55 +1100

    fix

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,13 +4,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideEchartsCore } from 'ngx-echarts';
 import * as echarts from 'echarts/core';
-import { RadarChart, HeatmapChart } from 'echarts/charts';
+import { RadarChart, BarChart } from 'echarts/charts';
 import { RadarComponent, TooltipComponent, LegendComponent, VisualMapComponent, GridComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth.interceptor';
 
-echarts.use([RadarChart, HeatmapChart, RadarComponent, TooltipComponent, LegendComponent, VisualMapComponent, GridComponent, CanvasRenderer]);
+echarts.use([RadarChart, BarChart, RadarComponent, TooltipComponent, LegendComponent, VisualMapComponent, GridComponent, CanvasRenderer]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
